379b7b40cbd9bb152dc886bc97aa864624778f34
sauvikaktivo <sauvik@aktivolabs.com>
2022-05-29 08:17:14 +0530

    syntax analysis add remove call back hell

--- bot_server/ReadMe.md
+++ bot_server/ReadMe.md
@@ -1,5 +1,3 @@
-
-
 ```
 {
     "codes": [
@@ -19,5 +17,129 @@
     "req": {
         "text": "sleep"
     }
+}
+```
+
+```
+sudo kill -9 `sudo lsof -t -i:3000`
+```
+
+```
+{
+    "KeyPhrases": [
+        {
+            "Score": 0.9999569654464722,
+            "Text": "my weight",
+            "BeginOffset": 14,
+            "EndOffset": 23
+        },
+        {
+            "Score": 0.988757312297821,
+            "Text": "26th",
+            "BeginOffset": 27,
+            "EndOffset": 31
+        }
+    ]
+}
+```
+
+```
+{
+    "Entities": [
+        {
+            "Score": 0.9894523620605469,
+            "Type": "DATE",
+            "Text": "26th may",
+            "BeginOffset": 27,
+            "EndOffset": 35
+        }
+    ]
+}
+```
+
+```
+{
+    "SyntaxTokens": [
+        {
+            "TokenId": 1,
+            "Text": "want",
+            "BeginOffset": 0,
+            "EndOffset": 4,
+            "PartOfSpeech": {
+                "Tag": "VERB",
+                "Score": 0.995025634765625
+            }
+        },
+        {
+            "TokenId": 2,
+            "Text": "to",
+            "BeginOffset": 5,
+            "EndOffset": 7,
+            "PartOfSpeech": {
+                "Tag": "PART",
+                "Score": 0.9999856948852539
+            }
+        },
+        {
+            "TokenId": 3,
+            "Text": "enter",
+            "BeginOffset": 8,
+            "EndOffset": 13,
+            "PartOfSpeech": {
+                "Tag": "VERB",
+                "Score": 0.9999977350234985
+            }
+        },
+        {
+            "TokenId": 4,
+            "Text": "my",
+            "BeginOffset": 14,
+            "EndOffset": 16,
+            "PartOfSpeech": {
+                "Tag": "PRON",
+                "Score": 0.9999768733978271
+            }
+        },
+        {
+            "TokenId": 5,
+            "Text": "weight",
+            "BeginOffset": 17,
+            "EndOffset": 23,
+            "PartOfSpeech": {
+                "Tag": "NOUN",
+                "Score": 0.9998605251312256
+            }
+        },
+        {
+            "TokenId": 6,
+            "Text": "on",
+            "BeginOffset": 24,
+            "EndOffset": 26,
+            "PartOfSpeech": {
+                "Tag": "ADP",
+                "Score": 0.9905756711959839
+            }
+        },
+        {
+            "TokenId": 7,
+            "Text": "26th",
+            "BeginOffset": 27,
+            "EndOffset": 31,
+            "PartOfSpeech": {
+                "Tag": "NOUN",
+                "Score": 0.6818108558654785
+            }
+        },
+        {
+            "TokenId": 8,
+            "Text": "may",
+            "BeginOffset": 32,
+            "EndOffset": 35,
+            "PartOfSpeech": {
+                "Tag": "AUX",
+                "Score": 0.9970732927322388
+            }
+        }
+    ]
 }
 ```
--- bot_server/controllers/nlp/awscomprehend.js
+++ bot_server/controllers/nlp/awscomprehend.js
@@ -1,4 +1,6 @@
+const { IoTEvents } = require('aws-sdk');
 const AWS = require('aws-sdk');
+const { isRequired } = require('nodemon/lib/utils');
 
 AWS.config.loadFromPath('./config/credentials.json');
 
@@ -8,32 +10,107 @@ var comprehend = new AWS.Comprehend({
 })
 
 
-const detectKeyPhrases = async(text, completion) => {
+const nlpAnalyse = async(text, completion) => {
     // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/getting-started-nodejs.html
     // https://docs.aws.amazon.com/comprehend/latest/dg/supported-languages.html
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Comprehend.html#batchDetectKeyPhrases-property
-    const result = {}
+    const result = []
+    try {
+        const phraseDetectionResult = await comprehendDetectKeyPhrases(text)
+        const entityDetectionResult = await comprehendDetectEntities(text)
+        const syntaxDetectionResult = await comprehendDetectSyntax(text)
+
+        completion(null, {
+            phrases: phraseDetectionResult,
+            entities: entityDetectionResult,
+            syntaxes: syntaxDetectionResult
+        })
+    } catch (e) {
+        completion(e, null)
+    }
+}
+
+function comprehendDetectEntities(text) {
     const command = {
         Text: text,
         LanguageCode: 'en'
     }
-    comprehend.detectEntities(command, (err, data) => {
-        if (data) {
-            result.entities = data
-            comprehend.detectKeyPhrases(command, (err, data) => {
-                if(data) {
-                    result.phrases = data
-                    completion(null, result)
-                } else {
-                    completion(error, null)
+    return new Promise((resolve, reject) => {
+        if(text) {
+            comprehend.detectEntities(command, (err, data) => {
+                if(err) { reject(err) }
+                else {
+                    const result = []
+                    data.Entities.forEach(item => {
+                        result.push({
+                            text: item.Text,
+                            score: item.Score,
+                            type: item.Type
+                        })
+                    });
+                    resolve(result) 
                 }
             })
         } else {
-            completion(error, null)
+            reject('Invalid text entry')
+        }
+    })
+}
+
+function comprehendDetectKeyPhrases(text) {
+    const command = {
+        Text: text,
+        LanguageCode: 'en'
+    }
+    return new Promise((resolve, reject) => {
+        if(text) {
+            comprehend.detectKeyPhrases(command, (err, data) => {
+                if(err) { reject(err) }
+                else {
+                    const result = []
+                    data.KeyPhrases.forEach(item => {
+                        result.push({
+                            text: item.Text,
+                            score: item.Score
+                        })
+                    });
+                    resolve(result) 
+                }
+            })
+        } else {
+            reject('Invalid text entry')
+        }
+    })
+}
+
+function comprehendDetectSyntax(text) {
+    const command = {
+        Text: text,
+        LanguageCode: 'en'
+    }
+    return new Promise((resolve, reject) => {
+        if(text) {
+            comprehend.detectSyntax(command, (err, data) => {
+                if(err) {reject(err)}
+                else { 
+                    const result = []
+                    data.SyntaxTokens.forEach(item => {
+                        result.push({
+                            tokenId: item.TokenId,
+                            text: item.Text,
+                            partOfSpeech: item.PartOfSpeech.Tag,
+                            score: item.PartOfSpeech.Score
+                        })
+                    });
+                    resolve(result) 
+                }
+            })
+        } else {
+            reject('Invalid text entry')
         }
     })
 }
 
 module.exports = {
-    detectKeyPhrases,
+    detectKeyPhrases: nlpAnalyse,
 }
